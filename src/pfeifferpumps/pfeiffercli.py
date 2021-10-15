83a90e1b8ec47c18de8c8de311c60ce945358218
tspspi <11219971+tspspi@users.noreply.github.com>
2021-10-15 13:20:04 +0200

    Add minimal sniffer CLI utility

--- src/pfeifferpumps/pfeiffercli.py
+++ src/pfeifferpumps/pfeiffercli.py
@@ -1,30 +1,47 @@
 import sys
 import serial
+import argparse
 
-from pfeifferproto import PfeifferProtocol
+from pfeifferproto import PfeifferProtocol, SerialProtocolViolation, SerialCommunicationError
 from pfeifferrs485 import PfeifferRS485Serial
 
 def pfeifferSnifferCLI():
-    if len(sys.argv) < 1:
-        printUsage()
-        return 1
+    ap = argparse.ArgumentParser(description = 'Simple access to Pfeiffer pumps on an RS485 bus attached to a serial port')
+    ap.add_argument('-p', '--port', type=str, required=False, default="/dev/ttyU0", help="Serial port to be used to access the RS485 bus")
+    ap.add_argument('-d', '--device', type=str, required=False, default=None, action='append', help="Adds a device registerset to a given address (ADR:DEVTYPE). Can be used multiple times")
+    args = ap.parse_args()
 
-    portFile = '/dev/ttyU0'
+    serialPort = args.port
+    regsets = { }
+    for devspec in args.device:
+        devspecparts = devspec.split(':')
+        if len(devspecparts) != 2:
+            print("Invalid device address : name specification {}".format(devspec[0]))
+            exit(1)
 
-    if len(sys.argv) > 1:
-        portFile = sys.argv[1]
+        try:
+            adr = int(devspecparts[0])
+        except ValueError:
+            print("Invalid device address {}".format(devspec[0]))
+            exit(1)
+        regsets[adr] = devspecparts[1]
 
     try:
-        with PfeifferRS485Serial(portFile) as port:
-            nextLine = serialNextLine(port)
-            packet = proto.decodePacketRaw(nextLine)
+        with PfeifferRS485Serial(serialPort, regsets) as port:
+            # nextLine = serialNextLine(port)
+            # packet = proto.decodePacketRaw(nextLine)
 
             # Currently decode everything according to TC110 register map
-
-            packet = proto.decodePacket(packet, proto.registersTC110)
-
+            # packet = proto.decodePacket(packet, proto.registersTC110)
+            nextMsg = port.nextMessage()
+            print(nextMsg)
+            pass
     except serial.SerialException as e:
         print("Failed to connect to serial port {}".format(portFile))
+    except SerialProtocolViolation as e:
+        print(e)
+    except SerialCommunicationError as e:
+        print(e)
     except KeyboardInterrupt:
         print("\r", end="")
         print("Exiting ...")
